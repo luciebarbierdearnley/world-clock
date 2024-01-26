84d6cca9969faa0969beefb93e4168e593cb0004
luciebarbierdearnley <barbier.lucie1@gmail.com>
2024-01-26 13:54:18 +0000

    Added timezones

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
-function showTime(event) {
+//function showTime(event) {
+
+//}
+
+//let chooseCity = document.querySelector("#cities");
+//chooseCity.addEventListener("change", showTime);
+
+function refreshTime() {
   let losAngelesElement = document.querySelector("#los-angeles");
   let losAngelesDateElement = losAngelesElement.querySelector(".date");
   let losAngelesTimeElement = losAngelesElement.querySelector(".time");
 
   let laCurrentDay = moment().tz("America/Los_Angeles").format("MMMM Do YYYY");
-  let laCurrentTime = moment().tz("America/Los_Angeles").format("h:mm:ss A");
+  let laCurrentTime = moment().tz("America/Los_Angeles");
 
-  if (event.target.value === "Los Angeles") {
-    losAngelesDateElement.innerHTML = laCurrentDay;
-    losAngelesTimeElement.innerHTML = laCurrentTime;
-  }
+  losAngelesDateElement.innerHTML = laCurrentDay;
+  losAngelesTimeElement.innerHTML = laCurrentTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+  let NewYorkElement = document.querySelector("#new-york");
+  let NewYorkDateElement = NewYorkElement.querySelector(".date");
+  let NewYorkTimeElement = NewYorkElement.querySelector(".time");
+
+  let nyCurrentDay = moment().tz("America/New_York").format("MMMM Do YYYY");
+  let nyCurrentTime = moment().tz("America/New_York");
+
+  NewYorkDateElement.innerHTML = nyCurrentDay;
+  NewYorkTimeElement.innerHTML = nyCurrentTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
 }
 
-let chooseCity = document.querySelector("#cities");
-chooseCity.addEventListener("change", showTime);
+refreshTime();
+setInterval(refreshTime, 1000);
